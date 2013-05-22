a4030753c064818ac80b0fdf8d92cf94f7ac6b0a
roundrop <roundrop@gmail.com>
2013-05-22 17:29:07 +0900

    initial commit

---
+++ src/facebook4j/examples/signin/LogoutServlet.java
@@ -0,0 +1,17 @@
+package facebook4j.examples.signin;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 5357658337449255998L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect(request.getContextPath()+ "/");
+    }
+}
---
+++ src/facebook4j/examples/signin/PostServlet.java
@@ -0,0 +1,28 @@
+package facebook4j.examples.signin;
+
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PostServlet extends HttpServlet {
+    private static final long serialVersionUID = 4179545353414298791L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String message = request.getParameter("message");
+        Facebook facebook = (Facebook) request.getSession().getAttribute("facebook");
+        try {
+            facebook.postStatusMessage(message);
+        } catch (FacebookException e) {
+            throw new ServletException(e);
+        }
+        response.sendRedirect(request.getContextPath()+ "/");
+    }
+    
+}
